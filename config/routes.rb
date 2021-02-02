6c982e765cda1806ecc8b4c294517e39eca46896
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2021-02-02 16:23:45 +0900

    トップページ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books/index'
+  root to:  'books#index'
+  resources :books, only: :index
 end
---
+++ db/migrate/20210202064024_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title, null: false
+      t.string :author, null: false
+      t.integer :genre_id, null: false
+      t.text :summary, null: false
+      t.text :impressions, null: false
+      t.timestamps
+    end
+  end
+end
