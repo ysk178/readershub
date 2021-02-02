54eef452954b194d5ebfdd2694178c40d1dec303
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2021-02-02 20:55:57 +0900

    新規投稿ページ編集

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,18 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :author
+    validates :genre_id
+    validates :summary
+    validates :impressions
+    validates :image
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :genre_id, numericality: { other_than: 1 }
 end
