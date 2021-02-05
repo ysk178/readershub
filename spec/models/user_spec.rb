b911548f693c6c84ee0de784bc059445325e4b1c
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2021-02-05 10:25:32 +0900

    userモデルの単体テスト記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'sample@sample.com', password: 'sample1', password_confirmation: 'sample1')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+  end
 end
