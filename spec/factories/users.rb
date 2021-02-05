e9f7c57130d06e3b4c7ac8b4a85cf22e01842f96
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2021-02-05 10:41:56 +0900

    Fakerでの単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {'test@test.com'}
     password              {'test1'}
     password_confirmation {password}
+
+    #nickname              {Faker::Name.initials(number: 2)}
+    #email                 {Faker::Internet.free_email}
+    #password              {Faker::Internet.password(min_length: 6)}
+    #password_confirmation {password}
   end
 end
